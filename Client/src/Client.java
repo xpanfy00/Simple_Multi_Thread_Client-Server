8f21bae5fc24714e507195ef530fefe32444c719
xpanfy00 <74433956+xpanfy00@users.noreply.github.com>
2021-03-21 01:40:38 +0100

    Initial commit

---
+++ Client/src/Client.java
@@ -0,0 +1,24 @@
+import org.weather.Phone;
+import java.io.*;
+
+
+
+public class Client {
+
+    public static void main(String[] args) {
+        try (Phone phone = new Phone("127.0.0.1", 8000)) {
+            System.out.println("Connected to server");
+            String request = "Brno";
+            System.out.println("Request " + request);
+
+            phone.writeLine(request);
+            String response = phone.readLine();
+            System.out.println("response " + response);
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
